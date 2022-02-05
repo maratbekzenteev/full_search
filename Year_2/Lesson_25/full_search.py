4b76df42eac2c3df3c0503dee2f3353b37a2b9be
marat <maratbekzenteev@yandex.ru>
2022-02-05 18:33:22 +0300

    second commit

--- Year_2/Lesson_25/full_search.py
+++ Year_2/Lesson_25/full_search.py
@@ -29,8 +29,6 @@ json_response = response.json()
 # Получаем первый топоним из ответа геокодера.
 toponym = json_response["response"]["GeoObjectCollection"][
     "featureMember"][0]["GeoObject"]
-print(json_response["response"]["GeoObjectCollection"][
-    "featureMember"][0])
 # Координаты центра топонима:
 toponym_coodrinates = toponym["Point"]["pos"]
 # Долгота и широта:
